9c6ae8b46297c6ad80c9c82670ea9ebdd40f63d8
Maneth-ii <manethbandara25@gmial.com>
2023-05-31 20:54:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef, useState } from 'react'
+import './index.css'
+
+function App() {
+const [data , setData] = useState('')
+
+    useEffect(()=> {
+      fetch('https://dummyjson.com/quotes/random')
+      .then(res => res.json())
+      .then((data) => setData(data));
+    },[])
+  
+
+
+  return(
+    <React.Fragment>
+
+<p className='text-sm justify-self-end'>created by Maneth thimasha</p>
+      <div className='flex flex-col w-100 mx-auto text-4xl mt-[15rem] justify-center items-center'>
+        <h1 className='text-white drop-shadow-3xl text-center'><p>" {data.quote} "</p></h1>
+        <p className='mt-4 self-end mr-16'><p>' {data.author} '</p></p>
+      </div>
+    </React.Fragment>
+  )
+  
+}
+
+export default App
